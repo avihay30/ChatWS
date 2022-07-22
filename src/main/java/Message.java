afd65ecf77677e9b510631bf9ee3f746afd5e6a2
Avihay Hadad <avihay30@gmail.com>
2022-07-22 11:36:54 +0300

    added base functionlity for server WS

---
+++ src/main/java/Message.java
@@ -0,0 +1,13 @@
+public class Message {
+    private String from;
+    private String to;
+    private String content;
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
